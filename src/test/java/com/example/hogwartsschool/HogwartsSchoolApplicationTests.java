70444ee6c9c32147bf26190663947f0584708b55
Аня <grigorevna.antoninka@mail.ru>
2023-06-26 12:50:27 +0700

    Initial commit

---
+++ src/test/java/com/example/hogwartsschool/HogwartsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogwartsschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
